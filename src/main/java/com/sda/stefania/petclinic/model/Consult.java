39415e7c87bdfa7cfee34a872ec1f018623a6f6e
Stefania <tudorica.stefania@gmail.com>
2022-05-16 20:00:26 +0300

    PCMS-12 Define hibernate database model

---
+++ src/main/java/com/sda/stefania/petclinic/model/Consult.java
@@ -0,0 +1,85 @@
+package com.sda.stefania.petclinic.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "consult")
+public class Consult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "veterinarian_id")
+    private Veterinarian veterinarian;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    public Consult() {
+    }
+
+    public Consult(Date date, String description) {
+        this.date = date;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Veterinarian getVeterinarian() {
+        return veterinarian;
+    }
+
+    public void setVeterinarian(Veterinarian veterinarian) {
+        this.veterinarian = veterinarian;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    @Override
+    public String toString() {
+        return "Consult{" +
+                "id=" + id +
+                ", date=" + date +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
